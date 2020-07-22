84f82bda33155d73927b38bbbbf4f6ecdf9c38d8
Sumtudou <1163015195@qq.com>
2020-07-22 20:45:47 +0800

    [OO Step by Step][Jay] feature description:finish 9th task

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.util.*;
+
 public class Klass {
+    private Integer number;
+    private Student leader = null;
+    private Set<Student> inClassStudent = new HashSet<>();
+
+    public void appendMember(Student stu){
+        inClassStudent.add(stu);
+    }
+
+    private boolean isStudentInClass(Student student){
+        return inClassStudent.contains(student);
+    }
+
+    public void assignLeader(Student stu){
+        if(isStudentInClass(stu))
+            leader = stu;
+        else
+            System.out.print("It is not one of us.\n");
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
 }
